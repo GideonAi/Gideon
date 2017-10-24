c73e03ae2257eedb57dc2dae4caba9292f457a3e
Joseph Robinson <smartech56@gmail.com>
2017-10-24 14:42:31 -0700

    Update setup.sh

--- Info/setup.sh
+++ Info/setup.sh
@@ -1,1 +1,13 @@
 echo "Still in beta..."
+echo starting...
+sudo su
+# Script from ServerBuilder (github.com/josephworks/serverbuilder)
+#-----------------------------------------------
+echo installing dependencies
+# main work
+sudo apt update
+sudo apt install toilet
+# Gideon depends
+echo installing dependencies ...
+sudo apt install wget apache2 mysql-server php5 libapache2-mod-php5 default-jre screen git curl unzip
+#-----------------------------------------------
