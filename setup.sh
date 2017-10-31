7ef1189aecdcfe7a327cc9a388941cabf8100c4c
Joseph Robinson <smartech56@gmail.com>
2017-10-30 19:31:01 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,8 +9,15 @@ sudo apt update
 sudo apt install toilet
 # Gideon depends
 echo installing dependencies ...
-sudo apt install wget git curl unzip python-pip python-dev build-essential 
+sudo apt install wget git curl unzip python-pip python-dev build-essential
+echo "Setting up python"
 sudo pip install --upgrade pip
 sudo pip install --upgrade virtualenv
-pip3 install pyvmomi
+# pip3 install pyvmomi
+echo "Setting up Gideon Webserver"
+sudo apt-get install apache2 unzip wget
+# cd /var/www/
+# wget https://github.com/GideonAi/web-speech-ai/archive/master.zip
+# mv master.zip www.zip
+# unzip www
 #-----------------------------------------------
